3ee8a03a55b49a91647772bb5dc46cb7c2ef2f1b
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-10-05 13:15:06 +0000

    added hello world

--- program.py
+++ program.py
@@ -1,9 +1,8 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def my_function(name):
-  myfunction = "Hello" + name
+def myfunction(Orion):
+  myfunction = "Hello" + Orion
   return myfunction
 
-name = "World!"
-print(my_function(name))
+print(myfunction("Orion"))
